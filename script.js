c41a2f8a7d849022b90362b22097b898f2eb8bc6
xizuyu <ejqtbrillo10@gmail.com>
2025-09-04 11:44:56 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you all the time.\n\nWe both tried. We both cared. But still, i failed to keep you, I ruined something so good and there's no one to blame but me.\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore. Please take care of yourself, I'll love you forever.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,3 +21,4 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
